201d86612af222b2045a646e2e46d0dad22e823a
Jim Nevin <jimnevin4@eircom.net>
2016-04-08 17:39:45 +0100

    Added new controller class for tidesrosslare

---
+++ application/controllers/tidesrosslare.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tidesrosslare extends CI_Controller {
+	
+	
+	/**
+	 * Index page for tides rosslare
+	 */
+	public function index() {
+		
+		echo "Tides Rosslare";
+		
+	}
+	
+	
+	
+	
+}
+
+
+
+?>
